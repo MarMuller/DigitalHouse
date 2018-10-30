3e76cfcdf64aa3c7b6755b2c9d8c8170c9602518
Marina <muller.marina@hotmail.com>
2018-10-30 12:03:52 -0300

    Octubre 30, 2018 (lista en progreso)

--- 08-EVENTOS-Y-TIMERS/practica-4/js/app.js
+++ 08-EVENTOS-Y-TIMERS/practica-4/js/app.js
@@ -29,7 +29,8 @@ window.onload = function() {
     do {
       var li=document.createElement('li');
       li.innerHTML = prompt("Ingrese el contenido del "+(i+1)+"° item.");
-    } while (li == "");
+      //li.setAttribute('class', 'clickeable');
+    } while (li.innerHTML == "");
     ul.appendChild(li);
   }
 
@@ -46,5 +47,28 @@ window.onload = function() {
   var parrafo = document.createElement('p');
   parrafo.innerText = "Tareas realizadas: 0";
   document.body.appendChild(parrafo);
+
+  /*-------*/
+
+  /*
+  Una vez con los <​ li>​ en nuestro documento, vamos a permitir que cada uno
+  de ellos pueda ser clicado y que al ser clicado, el mismo desaparezca después
+  de medio segundo. Así mismo, en el párrafo que dice ​ "Tareas
+  completadas: 0" ​ el número 0 deberá aumentar en 1 por cada tarea
+  eliminada.
+  */
+
+  var itemClickeable = document.querySelectorAll('li');
+
+  console.log(itemClickeable);
+
+  for (item of itemClickeable) {
+    //item.style.background = 'red';
+    item.onclick = function() {
+      console.log('hiciste el click');
+      item.style.display = 'none';
+    }
+  }
+
 
 }
