61f158f5daf18cd4b88d4528250afb1f3074d994
Marina <muller.marina@hotmail.com>
2018-11-08 23:01:09 -0300

    prueba casi lista

--- ZZ-ACTIVIDAD/JS-TEST/js/app.js
+++ ZZ-ACTIVIDAD/JS-TEST/js/app.js
@@ -2,25 +2,50 @@
 // text.classList.add("texto");
 // contenedor.appendChild(text);
 
+var contenedor = document.querySelector('.contenedor');
+var divs = [];
+
 function agregarDiv() {
-  var contenedor = document.querySelector('.contenedor');
-  var text = document.createElement('div');
-  text.classList.add("texto");
-  contenedor.appendChild(text);
-  console.log('ahi va');
+
+  var enlace = document.createElement('a');
+  var cuadro = document.createElement('div');
+  cuadro.classList.add("texto");
+  enlace.appendChild(cuadro);
+
+  divs.push(enlace);
+  //contenedor.innerHTML = divs;
+
+  for (item of divs) {
+    console.log(item);
+    contenedor.appendChild(item);
+  }
+
+  console.log('added');
 }
 
 function borrarDiv() {
-  var contenedor = document.querySelector('.contenedor');
-  if (contenedor != null) {
-    var contenedor = document.createElement('div');
-    document.body.appendChild(contenedor);
+  divs.pop();
+  contenedor.innerHTML = "";
+
+  for (item of divs) {
+    console.log(item);
+    contenedor.appendChild(item);
   }
-  var clear = document.querySelector('div');
-  if (clear != null) {
-    clear.remove();
-    console.log('clear');
-  }
+
+  console.log('deleted');
 }
+
+
+  // var contenedor = document.querySelector('.contenedor');
+  // if (contenedor != null) {
+  //   var contenedor = document.createElement('div');
+  //   document.body.appendChild(contenedor);
+  // }
+  // var clear = document.querySelector('div');
+  // if (clear != null) {
+  //   clear.remove();
+  //   console.log('clear');
+  // }
+
 
 //console.log('LISTO!');
