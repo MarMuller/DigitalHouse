e2360d6d8c4a5661da2d804881fa70bb5c7497a4
Marina <muller.marina@hotmail.com>
2018-11-09 10:47:54 -0300

    DESAFIO PULIDO

--- ZZ-ACTIVIDAD/JS-COLOR-PICKER/js/app.js
+++ ZZ-ACTIVIDAD/JS-COLOR-PICKER/js/app.js
@@ -1,50 +1,42 @@
-// var text = document.createElement('div');
-// text.classList.add("texto");
-// contenedor.appendChild(text);
-
 var contenedor = document.querySelector('.contenedor');
 var colorPicker = document.querySelector('.colorPicker');
 var choosenColor;
-var arrayColorPickers = [];
 var divs = [];
 
+//Toma color del colorPicker
 function tomarColor(x) {
   var color = x.style.backgroundColor;
   x.setAttribute("class", "choosen");
-  // x.class = "choosen";
-  //
-  // for (picker of arrayColorPickers) {
-  //   if (picker.class != "choosen") {
-  //     picker.class = "notChoosen";
-  //     picker.style.border = "1px solid black";
-  //   }
-  // }
-  //
-  // x.style.border = "2px solid white";
   choosenColor = color;
   //console.log(x);
   console.log(color);
-  //console.log(arrayColorPickers);
+
+  resetToNotChoosen();
+  x.setAttribute("class", "picker choosen");
+  changeStyleChoosen(x);
 }
 
+//Cambia color del circulo elegido
 function cambiarColor(x) {
   x.style.backgroundColor = choosenColor;
   //console.log(x);
 }
 
-//colorPicker
+//Crea colorPicker
 var colores = ["red","purple","blue","green","yellow","black","gray","white"];
 for (color of colores) {
   var picker = document.createElement('div');
   picker.setAttribute("style", 'background-color:'+color+'; width:25px; height:25px; border: 1px solid black; cursor:pointer; display:inline-block;');
   picker.setAttribute("onclick", "tomarColor(this)");
-  //picker.setAttribute("class", "notChoosen");
+  picker.classList.add("picker");
+  picker.classList.add("notChoosen");
+  //picker.setAttribute("class", "picker notChoosen");
   colorPicker.appendChild(picker);
   //arrayColorPickers.push(picker);
 }
 
+//Crea circulos
 function agregarDiv() {
-
   var cuadro = document.createElement('div');
   cuadro.setAttribute("style", "background-color:white; width:100px; height:100px; border: 1px solid black; border-radius: 50px; cursor:pointer; display:inline-block;");
   cuadro.setAttribute("onclick", "cambiarColor(this)");
@@ -55,18 +47,44 @@ function agregarDiv() {
   // for (item of divs) {
   //   console.log(item);
   // }
-
   console.log('added');
 }
 
+//Borra circulos
 function borrarDiv() {
   divs.pop();
   contenedor.innerHTML = "";
-
   for (item of divs) {
     //console.log(item);
     contenedor.appendChild(item);
   }
-
   console.log('deleted');
+}
+
+//Cambiar todos a notChoosen
+function resetToNotChoosen(){
+  var pickers = document.querySelectorAll(".picker");
+
+  for (picker of pickers) {
+    if ( picker.classList.contains("choosen") ) {
+      picker.setAttribute("class", "picker notChoosen");
+    }
+  }
+}
+
+//Cambiar estilo de choosen
+function changeStyleChoosen(n){
+  var pickers = document.querySelectorAll(".picker");
+
+  for (picker of pickers) {
+    if ( picker.classList.contains("choosen") ) {
+      if ( n.style.backgroundColor == "white") {
+        picker.style.border = "1px solid gray";
+      } else {
+        picker.style.border = "1px solid white";
+      }
+    } else {
+      picker.style.border = "1px solid black";
+    }
+  }
 }
